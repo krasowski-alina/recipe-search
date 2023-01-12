cfe52c306d1e357b4d2e4e8a3a0b323797616f03
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-01-12 15:00:37 -0500

    implemented serch function,fixed bugs related to the map method in both components(App.js and MyRecipesComponent.js),added styles to the List of ingredients

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import video from './food.mp4';
 import icon from './search.png';
 import { useEffect, useState } from 'react';
@@ -8,15 +7,16 @@ import MyRecipesComponent from './MyRecipesComponent';
 function App() {
   const MY_ID = "c47fa7e3";
   const MY_KEY ="c38b7932774a3764ceedd1ee8157afac"
-  const  [ mySearch, setMySearch ] = useState("");
-  const [myRecipe, setMyRecipe] = useState([])
+  const [mySearch, setMySearch ] = useState("");
+  const [myRecipe, setMyRecipe] = useState([]);
+  const [word, setWord] = useState("avocado")
 
   useEffect(()=> {
     findRecipe()
-  },[])
+  },[word])
 
 const findRecipe = async () =>{
-const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=salmon&app_id=${MY_ID}&app_key=${MY_KEY}`);
+const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${word}&app_id=${MY_ID}&app_key=${MY_KEY}`);
 const data = await response.json()
 setMyRecipe(data.hits)
 console.log(data.hits)
@@ -24,6 +24,10 @@ console.log(data.hits)
 const myRecipeSearch = (e) => {
 setMySearch(e.target.value)
 console.log(e.target.value)
+}
+const preventDownload = (e) => {
+  e.preventDefault();
+  setWord(mySearch)
 }
   return (
     <div className="App">
@@ -34,22 +38,21 @@ console.log(e.target.value)
         <h1>Find a Recipe</h1>
       </div>
       <div className="container">
-        <form>
+        <form onSubmit={preventDownload}>
           <input className="input" placeholder="Search..." onChange={myRecipeSearch} value={mySearch}></input>
         </form>
       </div>
       <div className="container">
-          <img src={icon} alt="search" width="50px"/>
+          <img 
+            src={icon} alt="search" width="50px"/>
       </div>
     <div>
-      {myRecipe.map((element => {
-        return(
-          <MyRecipesComponent label={element.recipe.label} calories={element.recipe.calories} image={element.recipe.image}/>
-        )
-      }))}
-   {/* {myRecipe.map(element => (
-    <MyRecipesComponent/>
-   ))} */}
+    {myRecipe.map((element, index) => (
+    <MyRecipesComponent key={index} label={element.recipe.label}
+    calories={element.recipe.calories} 
+    image={element.recipe.image}
+    ingredients={element.recipe.ingredientLines}/>
+    ))}
     </div>
     
 
--- src/MyRecipesComponent.js
+++ src/MyRecipesComponent.js
@@ -1,10 +1,17 @@
-
-export default function MyRecipesComponent({label, calories, image}){
+import check from "./check.png"
+export default function MyRecipesComponent({label, calories, image, ingredients}){
     return(
-        <div>
+        <div className="wrapper">
             <h2>{label}</h2>
-            <h3>{calories}</h3>
+            <h3>{calories.toFixed()} Calories</h3>
             <img src={image} alt="food"/>
+            <ul className="list">
+                {ingredients.map((ingredient, index) => (
+                    <li key={index}>
+                        <img src={check} alt="check mark" className="icon"/>
+                    {ingredient}</li>
+                ))}
+            </ul>
         </div>
     )
 }
